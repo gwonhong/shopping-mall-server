56a5f31ec494107d503b83b4d181ebcf89cd3755
Gwon Hong <gwonhong.dev@gmail.com>
2023-12-04 17:35:04 +0900

    UPDATE made simple test button to check if client communicates with server properly

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ import bodyParser from "body-parser";
 
 const app = express();
 app.use(bodyParser.json());
-
+app.get("/", (req, res, next) => {
+  res.send("hello from server!");
+});
 app.listen(8080, () => {
   console.log("listening on 8080...");
 });
