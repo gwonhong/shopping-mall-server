b7548482f0f3e8b7e2b2268536e1e7ad76386ad2
Gwon Hong <gwonhong.dev@gmail.com>
2023-12-06 17:56:31 +0900

    ADD update, delete product function

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -44,4 +44,44 @@ const createProduct = async (req, res, next) => {
   }
 };
 
-export default { getAllProducts, getProduct, createProduct };
+const updateProduct = async (req, res, next) => {
+  const token = req.body.token;
+  const productId = req.params.productId;
+  const { name, price, description } = req.body;
+  try {
+    const decoded = authenticateToken(token);
+    if (decoded.type !== "seller") throw new Error("Not a seller");
+
+    const product = await db.Product.findByPk(productId);
+    if (!product) throw new Error("Product Not Found");
+    if (product.sellerId !== decoded.id) throw new Error("Not your product");
+
+    product.set({ name, price, description });
+    const result = await product.save();
+    res.send(result);
+  } catch (err) {
+    console.error(err);
+    res.send(err);
+  }
+};
+
+const deleteProduct = async (req, res, next) => {
+  const token = req.body.token;
+  const productId = req.params.productId;
+  try {
+    const decoded = authenticateToken(token);
+    if (decoded.type !== "seller") throw new Error("Not a seller");
+
+    const product = await db.Product.findByPk(productId);
+    if (!product) throw new Error("Product Not Found");
+    if (product.sellerId !== decoded.id) throw new Error("Not your product");
+
+    const result = await product.destroy();
+    res.send(result);
+  } catch (err) {
+    console.error(err);
+    res.send(err);
+  }
+};
+
+export default { getAllProducts, getProduct, createProduct, updateProduct, deleteProduct };
--- server/routes/products.js
+++ server/routes/products.js
@@ -4,7 +4,8 @@ import productController from "../controllers/product.js";
 const router = express.Router();
 
 router.get("/:productId", productController.getProduct);
-router.put("/:productId");
+router.put("/:productId", productController.updateProduct);
+router.delete("/:productId", productController.deleteProduct);
 router.post("/", productController.createProduct);
 router.get("/", productController.getAllProducts);
 
